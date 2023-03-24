cc283535f699b3887b6cb2c92beeb0bbb4e8f85f
GizawAAiT <gizawaait@gmail.com>
2023-03-24 09:35:29 +0300

    I craeted New branch Gizaw

--- file1.py
+++ file1.py
@@ -1,1 +1,17 @@
-py
+def missingNumber( nums):
+    nums.append(-1) 
+    
+    def cycle_sort():
+        for indx in range(len(nums)):
+            while nums[indx] != indx : 
+                if nums[indx] == -1: break
+                nums[nums[indx]], nums[indx] = nums[indx], nums[nums[indx]]
+    
+    cycle_sort()
+    
+    for i in range(len(nums)):
+        if nums[i] == -1:
+            return i 
+    return nums
+print(missingNumber([3, 0, 1])) # output: 2
+
